6ab4aa33fbbdfccb43f22991e5c89351fa394535
titan9389 <titan9389@me.com>
2017-12-27 21:37:49 +0400

    init

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// SCRIPT FILE
+$(function() {
+
+	var $window = $(window);
+
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this);
+
+		$(window).scroll(function() {
+
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+			var coords = '50% '+ yPos + 'px';
+
+			$bgobj.css({ backgroundPosition: coords });
+
+		});
+	});
+
+});
